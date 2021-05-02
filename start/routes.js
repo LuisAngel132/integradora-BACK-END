734b5f73d24fbbb720fd7662881f87737df5660b
Your Name <you@example.com>
2021-05-02 13:23:34 -0500

    make it better

--- start/routes.js
+++ start/routes.js
@@ -16,4 +16,4 @@ Route.post('/plantafoto/:id','PlantaController.Foto')
 Route.delete('/logout','AuthController.logout')
 }).middleware('auth');
 Route.post('/reporte','ReporteController.store')
-Route.get('/reporte/','ReporteController.Userid')
+Route.get('/user/','ReporteController.Userid')
